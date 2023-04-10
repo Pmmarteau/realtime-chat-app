f0e77ced6370e0382f4adce2e59045e5bc3abf03
Pmmarteau <pm.marteau@gmail.com>
2023-04-10 22:20:51 +0200

    Add friend requests functionnality

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,16 +1,5 @@
-import Button from '@/components/UI/Button'
-import { authOptions } from '@/lib/auth'
-import { getServerSession } from 'next-auth'
-
-const Dashboard = async () => {
-  const session = await getServerSession(authOptions)
-
-  return (
-    <div>
-      <pre>{JSON.stringify(session)}</pre>
-      <Button>Hello</Button>
-    </div>
-  )
+const page = async () => {
+  return <div className="container py-12">Dashboard</div>
 }
 
-export default Dashboard
+export default page
